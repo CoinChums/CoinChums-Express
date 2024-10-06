61eca20fbb21f39feccf0770b8c7c4aa341be3df
Suraj <48823022+SurajGupta07@users.noreply.github.com>
2024-10-06 20:34:29 +0530

    migrate code (#7)

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,15 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @IsNotEmpty()
   readonly name: string;
 
   @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
 
   @IsString()
+  @IsNotEmpty()
   readonly password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,24 +2,25 @@ import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
-
-  @Get()
-  getUsers() {
-    return this.userService.getAll();
-  }
 
   @Get(':id')
   async getUser(@Param('id') movieId: string) {
     return await this.userService.getOne(movieId);
   }
 
-  @Post()
+  @Post('signup')
   async create(@Body() userData: CreateUserDto) {
     return await this.userService.create(userData);
+  }
+
+  @Post('login')
+  async login(@Body() userData: LoginUserDto) {
+    return await this.userService.login(userData);
   }
 
   @Patch(':id')
